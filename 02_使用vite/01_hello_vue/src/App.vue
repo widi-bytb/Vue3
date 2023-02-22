3f125249435ee14e43aeb7d6ff6550fefa56c1f8
widi-bytb <1679500573@qq.com>
2023-02-22 22:21:57 +0800

    单文件组件

---
+++ 02_使用vite/01_hello_vue/src/App.vue
@@ -0,0 +1,19 @@
+<script>
+import MyButton from "./components/MyButton.vue";
+
+export default {
+  data() {
+    return {
+      message: "你好Vue",
+    };
+  },
+  components: {
+    MyButton,
+  },
+};
+</script>
+
+<template>
+  <div>{{ message }}</div>
+  <MyButton></MyButton>
+</template>
---
+++ 02_使用vite/01_hello_vue/src/index.js
@@ -0,0 +1,5 @@
+import { createApp } from "vue";
+
+import App from "./App.vue";
+
+const vm = createApp(App).mount("#root");
